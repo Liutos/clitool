a93a91acdaa39961b7caf678b0f338ac53a8d0d9
LiuTao <mat.liutos@gmail.com>
2017-01-23 22:56:37 +0800

    除了返回一章的入口还需要返回章节名称

--- nodejs/genji-collection/worker/dir.js
+++ nodejs/genji-collection/worker/dir.js
@@ -11,7 +11,10 @@ function parse(content) {
     if ($a.text().indexOf('源君物语') !== -1) {
       const href = $a.attr('href');
       if (href) {
-        next.push('http://comic.kukudm.com' + href);
+        next.push({
+          dir: $a.text(),
+          entry: 'http://comic.kukudm.com' + href
+        });
       }
     }
   }
