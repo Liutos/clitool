3f99909335721939edc226f4c641c8a3acd9cf83
Liutos <mat.liutos@gmail.com>
2017-10-14 21:36:21 +0800

    将生成label的代码剥离为单独的函数

--- cl/defclass2dot/defclass2dot.lisp
+++ cl/defclass2dot/defclass2dot.lisp
@@ -1,14 +1,18 @@
 (in-package #:liutos.cli.defclass2dot)
 
-(defun defclass2dot (form
-                     &key (output *standard-output*))
-  "Convert FORM, which is a DEFCLASS form, to codes in DOT language."
+(defun defclass2label (form)
   (let ((name (nth 1 form))
         (direct-superclasses (format nil "~{~A~^,~}" (nth 2 form)))
         (direct-slots (mapcar #'first (nth 3 form))))
-    (format output "digraph g {~%")
-    (format output "    node0[label = \"~A | ~A |~{ ~A~^ |~}\", shape = \"record\"];~%"
+    (format nil "~A | ~A |~{ ~A~^ |~}"
             name
             direct-superclasses
-            direct-slots)
+            direct-slots)))
+
+(defun defclass2dot (form
+                     &key (output *standard-output*))
+  "Convert FORM, which is a DEFCLASS form, to codes in DOT language."
+  (let ((label (defclass2label form)))
+    (format output "digraph g {~%")
+    (format output "    node0[label = ~S, shape = \"record\"];~%" label)
     (format output "}~%")))
