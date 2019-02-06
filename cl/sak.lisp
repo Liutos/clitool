86d53b07aee1ce7974a78364c5ea5fe49ebe19ac
Liutos <mat.liutos@gmail.com>
2019-02-06 23:54:09 +0800

    新增宏：vertical-let，便于编写铜墙铁壁不需要频繁缩进的LET

--- cl/sak.lisp
+++ cl/sak.lisp
@@ -6,6 +6,40 @@
 
 (in-package #:com.liutos.sak)
 
+(defmacro vertical-let (&body body)
+  "不需要不停缩进的LET"
+  (let ((stack '()))
+    ;; 处理BODY中的所有元素将它们归类为binding和form塞入STACK中
+    (let ((lst body))
+      (while lst
+        (let ((e (pop lst)))
+          (cond ((eq e :with)
+                 (let* ((var (pop lst))
+                        (equal-sign (pop lst))
+                        (val (pop lst)))
+                   (assert (symbolp var))
+                   (assert (string= (symbol-name equal-sign) "="))
+                   (push (cons :binding (list var val)) stack)))
+                (t (push (cons :form e) stack))))))
+    ;; 处理STACK中的元素生成嵌套的LET表达式
+    (let ((bindings '())
+          (forms '())
+          (last-type nil))
+      (while stack
+        (let ((e (pop stack)))
+          (cond ((eq (car e) :binding)
+                 (push (cdr e) bindings)
+                 (setf last-type :binding))
+                (t
+                 (when (eq last-type :binding)
+                   (setf forms
+                         `((let ,bindings ,@forms)))
+                   (setf bindings '()))
+                 (push (cdr e) forms)
+                 (setf last-type :form)))))
+      `(let ,bindings
+         ,@forms))))
+
 (defmacro while (test &body body)
   "C语言风格的while循环"
   `(loop
