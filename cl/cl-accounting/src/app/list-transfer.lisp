836b7ca1460fe1f42557d5b6773ad5cdb7a7eea8
Liutos <mat.liutos@gmail.com>
2025-02-15 23:50:27 +0800

    refactor: 在转账列表中展示完整的账户层级。

--- cl/cl-accounting/src/app/list-transfer.lisp
+++ cl/cl-accounting/src/app/list-transfer.lisp
@@ -24,17 +24,27 @@
               (cl-accounting.entity:list-transfer transfer-repo)))
         (dolist (transfer transfer-list)
           (let* ((from-account-id (cl-accounting.entity:transfer-from-account-id transfer))
-                 (from-account (cl-accounting.entity:get-account account-repo from-account-id))
-                 (to-account-id (cl-accounting.entity:transfer-to-account-id transfer))
-                 (to-account (cl-accounting.entity:get-account account-repo to-account-id)))
+                 (to-account-id (cl-accounting.entity:transfer-to-account-id transfer)))
             (push (make-transfer-list-item
                    :amount (cl-accounting.entity:transfer-amount transfer)
                    :comment (cl-accounting.entity:transfer-comment transfer)
                    :created-at (cl-accounting.entity:transfer-created-at transfer)
-                   :from-account-name (cl-accounting.entity:account-name from-account)
+                   :from-account-name (gen-full-account-name from-account-id account-repo)
                    :id (cl-accounting.entity:transfer-id transfer)
-                   :to-account-name (cl-accounting.entity:account-name to-account))
+                   :to-account-name (gen-full-account-name to-account-id account-repo))
                   items)))
 
         ;; 最后返回被填充的结果。
         items))))
+
+(defun gen-full-account-name (account-id account-repo)
+  "生成完整层级的账户名称。"
+  (let* ((account (cl-accounting.entity:get-account account-repo account-id))
+         (account-name (cl-accounting.entity:account-name account))
+         (parent-id (cl-accounting.entity:account-parent-id account)))
+    (when (zerop parent-id)
+      ;; 已经是顶层了，那么账户的完整名称就是自己。
+      (return-from gen-full-account-name account-name))
+
+    (let ((parent-name (gen-full-account-name parent-id account-repo)))
+      (format nil "~A:~A" parent-name account-name))))
